43b506fc4cd2d6af2787b947b2a39ef4a02f3be7
Natsuki Wada <natsuki@MacBook-Air.lan>
2021-09-28 22:45:18 -0400

    added form

--- client/containers/HaikuContainer.jsx
+++ client/containers/HaikuContainer.jsx
@@ -1,16 +1,89 @@
-import React, { Component } from 'react';
-import WritingPane from '../components/WritingPane.jsx';
-import SaveButton from '../components/SaveButton.jsx';
-import PoemLineDisplay from '../components/PoemLineDisplay.jsx';
+import React, { useState, useEffect } from 'react';
 
-function HaikuContainer() {
+// const dataObj = {
+//   "_id": "6153ae6ec2eed9ce28d1f72f",
+//   "title": "Kitty",
+//   "textLine1": [
+//     "The",
+//     "cat",
+//     "is",
+//     "fluffy"
+//   ],
+//   "textLine2": [
+//     "Her",
+//     "soft",
+//     "fur",
+//     "shimmers",
+//     "in",
+//     "grey"
+//   ],
+//   "textLine3": [
+//     "She",
+//     "is",
+//     "Beyonce"
+//   ],
+//   "associatedUserID": "Matt17",
+//   "public": false,
+//   "rating": "",
+//   "gifs": [
+//     "",
+//     "",
+//     ""
+//   ],
+//   "__v": 0
+// }
+
+const WritingPane = ({ handleChange }) => {
   return (
-    <div className="App">
-      <WritingPane></WritingPane>
-      <PoemLineDisplay></PoemLineDisplay>
-      <SaveButton></SaveButton>
+    <div>
+      <div>Writing Pane</div>
+      <label>
+        <input name="title" type="text" placeholder="Title is here..." onChange={handleChange}/>
+      </label>
+      <div className="haiku-btn-wrap">
+        <button>5</button>
+        <button>7</button>
+        <button>5</button>
+      </div>
     </div>
-  );
+  )
+}
+
+const PoemLineDisplay = () => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  )
+}
+
+const SaveButton = ({ handleSave }) => {
+  return (
+    <div>
+      <button onClick={handleSave}>Save Haiku</button>
+    </div>
+  )
+}
+
+const HaikuContainer = () => {
+  const [title, setTitle] = useState('');
+  const handleChange = (e) => setTitle(e.target.value)
+
+  const handleSave = () => {
+    console.log('saved')
+  }
+
+  return (
+    <div className="haiku-container">
+      <WritingPane 
+        handleChange={handleChange}
+      />
+      <PoemLineDisplay />
+      <SaveButton 
+        handleSave={handleSave} 
+      />
+    </div>
+  )
 }
 
 export default HaikuContainer;
